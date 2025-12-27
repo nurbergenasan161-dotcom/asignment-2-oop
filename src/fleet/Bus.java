8e5073e6d7acd64f98358151d6f96ef928642f46
Nurbergen <nurbergenasan161@gmail.com>
2025-12-28 02:25:18 +0500

    fix vehicle class

--- src/fleet/Bus.java
+++ src/fleet/Bus.java
@@ -9,22 +9,17 @@ public class Bus extends Vehicle {
         this.capacity = capacity;
     }
 
-    @Override
     public double calculateInsuranceFee() {
         return basePrice * 0.1;
     }
 
-    @Override
     public void performService() {
         System.out.println("Bus service: " + model);
     }
-
-    @Override
     public int getServiceIntervalKm() {
         return 20000;
     }
 
-    @Override
     public String toString() {
         return super.toString() + " | capacity: " + capacity;
     }
--- src/fleet/Car.java
+++ src/fleet/Car.java
@@ -9,22 +9,19 @@ public class Car extends Vehicle {
         this.doors = doors;
     }
 
-    @Override
     public double calculateInsuranceFee() {
         return basePrice * 0.05;
     }
 
-    @Override
     public void performService() {
         System.out.println("Car service: " + model);
     }
 
-    @Override
     public int getServiceIntervalKm() {
         return 10000;
     }
 
-    @Override
+
     public String toString() {
         return super.toString() + " | doors: " + doors;
     }
